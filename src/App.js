3c823f02fa6a7d940cd11ff62730ffa4ed2b53ac
yadavraju <abdul.h@chapter247.com>
2022-01-27 18:49:10 +0530

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import ContactHeader from './components/ContactHeader.js';
 import AddContact from './components/AddContact.js';
@@ -6,12 +6,21 @@ import ContactList from './components/ContactList.js';
 
 
 function App() {
-
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
   };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts]);
 
   return (
     <div className="ui container">
